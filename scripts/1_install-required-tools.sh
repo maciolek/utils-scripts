7716cc3a37da2493c9e7c3ccbdbccfafe4dbbab6
Dariusz Maciołek <maciolek.dariusz@gmail.com>
2025-04-07 20:53:55 +0200

    add alias ll to 1 script

--- scripts/1_install-required-tools.sh
+++ scripts/1_install-required-tools.sh
@@ -15,6 +15,24 @@ install_if_missing() {
     fi
 }
 
+# Funkcja dodająca alias do .bashrc
+add_alias_to_bashrc() {
+    local alias_command=$1
+    local bashrc_path=~/.bashrc
+
+    if grep -qF "$alias_command" "$bashrc_path"; then
+        echo "Alias jest już dodany do .bashrc"
+    else
+        echo "Dodaję alias do .bashrc..."
+        echo "$alias_command" >> "$bashrc_path"
+        if [ $? -eq 0 ]; then
+            echo "Alias dodany pomyślnie"
+        else
+            echo "Błąd: Nie udało się dodać aliasu do .bashrc"
+        fi
+    fi
+}
+
 # Lista wymaganych narzędzi
 TOOLS=("curl" "wget" "vim" "git")
 
@@ -22,5 +40,9 @@ TOOLS=("curl" "wget" "vim" "git")
 for tool in "${TOOLS[@]}"; do
     install_if_missing "$tool"
 done
+
+# Dodanie aliasu
+alias_command="alias ll='ls -lh --color=auto'"
+add_alias_to_bashrc "$alias_command"
 
 echo "Wszystkie wymagane narzędzia są zainstalowane"
