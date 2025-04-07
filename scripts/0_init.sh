56fca27d2211c0a15fe698753283ae395eea9ff6
Dariusz Maciołek <maciolek.dariusz@gmail.com>
2025-04-07 19:27:37 +0200

    fix

---
+++ scripts/0_init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Konfiguracja
+USERNAME="maciolek"
+REPO="utils-scripts"
+BRANCH="init"
+TARGET_DIR="scripts"
+SCRIPT1="1_install-required-tools.sh"
+SCRIPT2="2_download-scripts.sh"
+
+# URL plików w formacie RAW
+URL1="https://raw.githubusercontent.com/${USERNAME}/${REPO}/${BRANCH}/${TARGET_DIR}/${SCRIPT1}"
+URL2="https://raw.githubusercontent.com/${USERNAME}/${REPO}/${BRANCH}/${TARGET_DIR}/${SCRIPT2}"
+
+# Pobierz pliki
+echo "Pobieranie plików..."
+curl -s -o "${SCRIPT1}" "${URL1}"
+curl -s -o "${SCRIPT2}" "${URL2}"
+
+# Sprawdź, czy pliki zostały pobrane
+if [ -f "${SCRIPT1}" ] && [ -f "${SCRIPT2}" ]; then
+    echo "Gotowe! Pliki zostały pobrane do katalogu głównego."
+    
+    # Nadaj uprawnienia 700
+    echo "Nadaję uprawnienia 700..."
+    chmod 700 "${SCRIPT1}"
+    chmod 700 "${SCRIPT2}"
+    
+    echo "Uprawnienia nadane."
+else
+    echo "Błąd: Nie udało się pobrać jednego lub obu plików."
+fi
